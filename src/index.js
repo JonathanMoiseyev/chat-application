22faaa0806397be9115fe1183322a99354078d0f
Yuval Grofman <yuvalgrofman@gmail.com>
2023-05-17 18:49:28 +0300

    changed update and index files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Register from './Register.js';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Register />
+        <App />
     </React.StrictMode>
 );
