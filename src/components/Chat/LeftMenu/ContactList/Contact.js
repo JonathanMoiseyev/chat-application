86abf575af53fb75ae9ff211aae5e6e086c578eb
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-05-17 22:33:22 +0300

    minor changes

--- src/components/Chat/LeftMenu/ContactList/Contact.js
+++ src/components/Chat/LeftMenu/ContactList/Contact.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-function Contact(props) {
+function Contact({img, name, lastMessage, date}) {
     return (
         <li className="list-group-item d-flex align-items-center mx-0 darken-on-hover">
             <div>
-                <img src={props.img} className="rounded-circle" alt="avatar" />
+                <img src={img} className="rounded-circle" alt="avatar" />
             </div>
             <div className="w-100 ms-4">
-                <div>{props.name}</div>
-                <small className="text-muted">{props.lastMessage}</small>
+                <div>{name}</div>
+                <small className="text-muted">{lastMessage}</small>
             </div>
             <div>
-                <small className="text-muted me-2">{props.date}</small>
+                <small className="text-muted me-2">{date}</small>
                 <span className="badge bg-light-purple rounded-pill float-end me-2">
                     14
                 </span>
--- src/db/contacs.js
+++ src/db/contacs.js
@@ -2,14 +2,17 @@ let contacts = [
     {
         img: "img/whale.jpeg",
         name: "Yuval Grofman",
+        unread: 2,
     },
     {
         img: "img/pelican.jpeg",
         name: "Jonathan Moiseyev",
+        unread: 1,
     },
     {
         img: "img/seal.jpeg",
         name: "Jonathn Kelsi",
+        unread: 0,
     }
 ];
 
