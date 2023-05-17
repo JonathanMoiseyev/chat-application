cfac801fcc8776229a0f8f951965d0bf9a7ac967
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-05-17 18:29:26 +0300

    finished search bar!

--- src/components/Chat/LeftMenu/LeftMenu.js
+++ src/components/Chat/LeftMenu/LeftMenu.js
@@ -6,10 +6,16 @@ import SearchBar from "./SearchBar/SearchBar.js";
 function LeftMenu(props) {
     const [effectiveContacts, setEffectiveContacts] = React.useState(props.contacts);
 
-    const doSearch = function(query) {  
-        setEffectiveContacts(props.contacts.filter((contact) => contact.name.includes(query)));
-    }
-    
+    const doSearch = function (query) {
+        setEffectiveContacts(
+            props.contacts.filter((contact) =>
+                query === undefined
+                    ? true
+                    : contact.name.toLowerCase().includes(query.toLowerCase())
+            )
+        );
+    };
+
     return (
         <div className="col-4 p-0 border-end">
             <div className="card border-0">
@@ -80,7 +86,7 @@ function LeftMenu(props) {
                         </ul>
                     </div>
                 </div>
-                <SearchBar doSearch={doSearch}/>
+                <SearchBar doSearch={doSearch} />
                 <ContactList contacts={effectiveContacts} />
             </div>
         </div>
