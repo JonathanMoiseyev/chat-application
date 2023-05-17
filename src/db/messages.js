9d331113cac69575d797eadb42183e1bd12307ab
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-05-18 02:21:23 +0300

    added messages

---
+++ src/db/messages.js
@@ -0,0 +1,40 @@
+let messages = {
+    "Yuval Grofman": [
+        {
+            content: "Hello, how are you?",
+            type: "recv",
+            date: new Date("2020-01-01T10:00:00")
+        },
+        {
+            content: "I'm fine, thanks!",
+            type: "sent",
+            date: new Date("2020-01-01T10:01:00")
+        }
+    ],
+    "Jonathan Moiseyev": [
+        {
+            content: "whatsssssuppppp?",
+            type: "recv",
+            date: new Date("2020-01-01T10:00:00")
+        },
+        {
+            content: "nothing much, you?",
+            type: "sent",
+            date: new Date("2020-01-01T10:01:00")
+        }
+    ],
+    "Jonathn Kelsi": [
+        {
+            content: "U up ?",
+            type: "recv",
+            date: new Date("2020-01-01T10:00:00")
+        },
+        {
+            content: "Yup",
+            type: "sent",
+            date: new Date("2020-01-01T10:01:00")
+        }
+    ]
+};
+
+export default messages;
