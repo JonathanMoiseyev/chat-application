104094008a7551c7cea92e86f7c9f72c57f02f2c
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-05-17 15:01:38 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Chat from './components/Chat/Chat';
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/chat" component={Chat} />
       </Switch>
     </BrowserRouter>
   );
