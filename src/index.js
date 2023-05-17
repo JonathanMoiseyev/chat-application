db6f39f38e4b97589615e9b1b091925b9e60b75c
Yuval Grofman <yuvalgrofman@gmail.com>
2023-05-17 17:43:07 +0300

    commented irrelevant part of code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import Register from './Register.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
