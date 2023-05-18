13e59ecbac2458c10c1cdcde9cfce24c01c2cf90
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-05-18 19:49:50 +0300

    started working on chat

--- src/components/Chat/ChatArea/ChatArea.js
+++ src/components/Chat/ChatArea/ChatArea.js
@@ -3,12 +3,12 @@ import TypingArea from "./TypingArea/TypingArea";
 import MessageBoard from "./MessageBoard/MessageBoard";
 import RecipientDetails from "./RecipientDetails/RecipientDetails";
 
-function ChatArea({user, chosenContact}) {
+function ChatArea({ user, chosenContact }) {
     return (
         <div className="col-8 p-0">
             <div className="card border-0">
-                <RecipientDetails chosenContact={chosenContact}/>           
-                <MessageBoard />
+                <RecipientDetails chosenContact={chosenContact} />
+                <MessageBoard user={user} chosenContact={chosenContact} />
                 <TypingArea />
             </div>
         </div>
--- src/components/Chat/ChatArea/MessageBoard/MessageBoard.js
+++ src/components/Chat/ChatArea/MessageBoard/MessageBoard.js
@@ -1,9 +1,19 @@
-import React from "react";
+import {useRef, useEffect} from "react";
+import chatsDB from "../../../../db/chatsDB";
 
-function MessageBoard(props) {
+function MessageBoard({user, chosenContact}) {
+    const chatRef = useRef(null);
+
+    useEffect(() => {
+        if (chosenContact != null) {
+            const messages = chatsDB[user.username][chosenContact];
+            // const messagesComponents = messages.map((message, key) => <Message {...message} key={key} />);
+        }
+    });
+
     return (
         <div className="card-body p-0">
-            <ul className="list-unstyled overflow-auto m-0 pt-2" id="chat"></ul>
+            <ul className="list-unstyled overflow-auto m-0 pt-2" id="chat" ref={chatRef}></ul>
         </div>
     );
 }
