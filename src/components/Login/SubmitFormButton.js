6d751f7e6251a4eee125776fbb06d1336bdf41a1
Jonathan Moiseyev <jonathanmoiseyev@outlook.com>
2023-05-18 18:52:42 +0300

    made the login page check if the password is correct

--- src/components/Login/SubmitFormButton.js
+++ src/components/Login/SubmitFormButton.js
@@ -1,10 +1,13 @@
-function SubmitFormButton({writingOnButton}) {
+import React from "react";
+
+function SubmitFormButton({writingOnButton, signInFunction}) {
   return (
     <button
         type="submit"
+        onClick={signInFunction}
         className="btn bg-light-purple darken-on-hover w-100 text-white fw-600 py-2 mb-4"
     >
-        {writingOnButton}
+        {writingOnButton}        
     </button>
   );
 }
