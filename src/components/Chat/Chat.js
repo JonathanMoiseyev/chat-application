b0159737c833e5eacf1e19aa80c0aa5a8f9ab1d0
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-05-19 16:27:57 +0300

    basic chat behavior added

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,13 +4,23 @@ import LeftMenu from "./LeftMenu/LeftMenu";
 import ChatArea from "./ChatArea/ChatArea";
 
 function Chat({ user, setUser }) {
+    const [status, forceUpdate] = useState(false);
     const [chosenContact, setChosenContact] = useState(null);
 
     return (
         <main className="container shadow mt-4" id="chat-app">
             <div className="row">
-                <LeftMenu user={user} setUser={setUser} setChosenContact={setChosenContact} />
-                <ChatArea user={user} chosenContact={chosenContact} />
+                <LeftMenu
+                    user={user}
+                    setUser={setUser}
+                    setChosenContact={setChosenContact}
+                />
+                <ChatArea
+                    user={user}
+                    chosenContact={chosenContact}
+                    forceUpdate={forceUpdate}
+                    status={status}
+                />
             </div>
         </main>
     );
