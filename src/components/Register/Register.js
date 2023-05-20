b31bf69a52397a398a6dcec3f44f3f32316e894e
Jonathan Moiseyev <jonathanmoiseyev@outlook.com>
2023-05-20 18:04:48 +0300

    removed unnecessery files

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ImgField from './ImgField.js';
 import InputField from './InputField.js'; 
-// import RememberMeButton from './RememberMeButton.js';
 import Link from '../shared/Link.js';
 import './Register.css';
 import '../shared/PasswordInputField/PasswordInputField.js';
