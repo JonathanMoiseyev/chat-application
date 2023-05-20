a9e691fa9017b57412e981466efcfc1d3ba8cb4e
Jonathan Moiseyev <jonathanmoiseyev@outlook.com>
2023-05-20 16:37:06 +0300

    fixed the new password button border highlight when onFocus

--- src/components/shared/PasswordInputField/PasswordInputField.js
+++ src/components/shared/PasswordInputField/PasswordInputField.js
@@ -1,12 +1,12 @@
 import {useRef} from "react";
 import {useState} from "react";
 import ButtonToShowPassword from "./ButtonToShowPassword";
+import "./PasswordInputField.css";
 
 function PasswordInputField({labelOfInputField, idOfInputField, updateFunction}) {
     const inputReference = useRef(null);
     
     const [inputType, setInputType] = useState("password");
-    
     const showOrHidePassword = () => {
         if (inputType === "password") {
             setInputType("text");
@@ -17,30 +17,43 @@ function PasswordInputField({labelOfInputField, idOfInputField, updateFunction})
 
 
 
+    const [classNameOfDivOfInputAndButton, setClassNameOfDivOfInputAndButton]
+                = useState("input-group form-control p-0");
+
+    const onFocusFunction = () => {
+        setClassNameOfDivOfInputAndButton("input-group form-control p-0 highlited-border");
+    }
+
+    const onBlurFunction = () => {
+        setClassNameOfDivOfInputAndButton("input-group form-control p-0");
+    }
+
+
+
+    
     return (
         <div className="fw-600 mb-4">
             <label htmlFor={idOfInputField} className="form-label fw-600">
                 {labelOfInputField}
             </label>
         
+            <div className="row">
+                <div className="col">
+                    <div className={classNameOfDivOfInputAndButton}>
+                        <input 
+                            type={inputType}
+                            className="form-control m-0 border-0"
+                            id={idOfInputField}
+                            ref={inputReference}
+                            onFocus={onFocusFunction}
 
-        <div className="row">
-            <div className="col">
-                <div className="input-group form-control p-0">
-                    <input 
-                        type={inputType}
-                        className="form-control m-0 border-0"
-                        id={idOfInputField}
-                        ref={inputReference}
-                
-                        onChange={() => updateFunction(inputReference.current.value)}
-                    />
-                    <div className="input-group-btn">
+                            onChange={() => updateFunction(inputReference.current.value)}
+                            onBlur={onBlurFunction}
+                        />
                         <ButtonToShowPassword showOrHidePassword={showOrHidePassword} />
                     </div>
                 </div>
             </div>
-        </div>
         </div>
   );
 }
