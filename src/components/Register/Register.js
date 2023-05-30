df5056e3a08c25f8c51e75b35ac584aceab8e010
yuval <yuvalgrofman@gmail.com>
2023-05-30 12:54:38 +0300

    reformated register.js and removed unnecessary comments

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import ImgField from './ImgField.js';
-import Link from '../shared/Link.js';
-import './Register.css';
-import '../shared/PasswordInputField/PasswordInputField.js';
-import PasswordInputField from '../shared/PasswordInputField/PasswordInputField.js';
-import InputField from '../shared/InputField.js';
+/** @format */
 
-// import chatsDB from '../../db/chatsDB.js';
-// import userDB from '../../db/usersDB.js';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ImgField from "./ImgField.js";
+import Link from "../shared/Link.js";
+import "./Register.css";
+import "../shared/PasswordInputField/PasswordInputField.js";
+import PasswordInputField from "../shared/PasswordInputField/PasswordInputField.js";
+import InputField from "../shared/InputField.js";
 
 /**
  * Regitser function returns the register page.
@@ -16,103 +15,86 @@ import InputField from '../shared/InputField.js';
  * the register page contains a link to the login page.
  * The forum validates the user input and if the input is valid
  * its saves the user in the usersDB.
- * @returns {HTMLDivElement} The register page. 
+ * @returns {HTMLDivElement} The register page.
  */
 function Register() {
     const navigate = useNavigate();
 
     const [user, setUser] = React.useState({
-        username: '',
-        password: '',
-        confirmPassword: '',
-        profilePic: '',
-        displayName: '',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        profilePic: "",
+        displayName: "",
     });
 
-    const [error, setError] = React.useState('');
+    const [error, setError] = React.useState("");
 
     /**
      * The function handles the change of the input fields.
      * Its updates the user state according to the input field.
-     * @param {*} name 
-     * @param {*} value 
+     * @param {*} name
+     * @param {*} value
      */
     const handleChange = (name, value) => {
         setUser({
             ...user,
             [name]: value,
         });
-    }
+    };
 
     /**
      * The function validates the user input and if the input is valid
-     * its saves the user in the usersDB and navigates to the login page. 
-     * @param {*} event 
+     * its saves the user in the usersDB and navigates to the login page.
+     * @param {*} event
      */
     const validateAndSubmit = (event, setError) => {
         event.preventDefault();
-        let error = '';
+        let error = "";
 
         if (!user.username) {
-            error= 'Username is required';
+            error = "Username is required";
         } else if (!user.password) {
-            error= 'Password is required';
+            error = "Password is required";
         } else if (!/\d/.test(user.password)) {
-            error= 'Password must contain at least one number';
+            error = "Password must contain at least one number";
         } else if (!/[a-zA-Z]/.test(user.password)) {
-            error= 'Password must contain at least one letter';
+            error = "Password must contain at least one letter";
         } else if (user.password.length < 8) {
-            error= 'Password must be at least 8 characters';
+            error = "Password must be at least 8 characters";
         } else if (!user.confirmPassword) {
-            error= 'Confirm password is required';
+            error = "Confirm password is required";
         } else if (user.password !== user.confirmPassword) {
-            error= 'Confirm password is not match';
+            error = "Confirm password is not match";
         } else if (!user.displayName) {
-            error= 'Display name is required';
+            error = "Display name is required";
         } else if (!user.profilePic) {
-            error= 'Picture is required';
+            error = "Picture is required";
         } else {
             handleSubmit(setError);
         }
 
         setError(error);
-    }
+    };
 
     const handleSubmit = async (setError) => {
         delete user.confirmPassword;
 
-        const response = await fetch('http://localhost:5000/api/Users', {
-            method: 'POST',
+        const response = await fetch("http://localhost:5000/api/Users", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(user),
         });
 
-        if (response['status'] === 409) {
-            setError('Username already exists');
+        if (response["status"] === 409) {
+            setError("Username already exists");
         } else {
-            setError('');
-            navigate('/');
+            setError("");
+            navigate("/");
         }
-    }
-
-    // const updateUserUsername = (value) => {
-    //     user.username = value;
-    // }
-
-    // const updatueUserConfirmPassword = (value) => {
-    //     user.confirmPassword = value;
-    // }
-
-    // const updatueUserPassword = (value) => {
-    //     user.password = value;
-    // }
-    
-    // const updateUserDisplayName = (value) => {
-    //     user.displayName = value;
-    // }
-
+    };
 
     return (
         <div className="p-5">
@@ -127,11 +109,13 @@ function Register() {
                                 {/*Signup form*/}
                                 <form>
                                     {/*Username input*/}
-                                    <InputField 
+                                    <InputField
                                         labelOfInputField="username"
                                         idOfInputField="register-username"
                                         // updateFunction={updateUserUsername}
-                                        updateFunction={(value) => { handleChange('username', value); }}
+                                        updateFunction={(value) => {
+                                            handleChange("username", value);
+                                        }}
                                         input-type="text"
                                     />
 
@@ -140,7 +124,9 @@ function Register() {
                                         labelOfInputField="password"
                                         idOfInputField="register-passwd"
                                         // updateFunction={updatueUserPassword}
-                                        updateFunction={(value) => { handleChange('password', value); }}
+                                        updateFunction={(value) => {
+                                            handleChange("password", value);
+                                        }}
                                     />
 
                                     {/*Confirm password input*/}
@@ -148,35 +134,53 @@ function Register() {
                                         labelOfInputField="confirm password"
                                         idOfInputField="register-confirm-passwd"
                                         // updateFunction={updatueUserConfirmPassword}
-                                        updateFunction={(value) => { handleChange('confirmPassword', value); }}
+                                        updateFunction={(value) => {
+                                            handleChange("confirmPassword", value);
+                                        }}
                                     />
 
                                     {/*Display name*/}
-                                    <InputField 
+                                    <InputField
                                         labelOfInputField="display name"
                                         idOfInputField="register-display-name"
                                         // updateFunction={updateUserDisplayName}
-                                        updateFunction={(value) => { handleChange('displayName', value); }}
+                                        updateFunction={(value) => {
+                                            handleChange("displayName", value);
+                                        }}
                                         input-type="text"
                                     />
 
                                     {/*Picture*/}
-                                    <ImgField name="profilePic" id="register-picture"
-                                        text="picture" value={user.picture}
-                                        handleChange={(value) => { handleChange('profilePic', value); }}
+                                    <ImgField
+                                        name="profilePic"
+                                        id="register-picture"
+                                        text="picture"
+                                        value={user.picture}
+                                        handleChange={(value) => {
+                                            handleChange("profilePic", value);
+                                        }}
                                     />
-
 
                                     {/*Submit and redirection to sign in*/}
                                     <div className="d-flex flex-column">
                                         <p className="error-message bold mx-auto mb-3">{error}</p>
 
-                                        <button type="submit" onClick={(event) => { validateAndSubmit(event, setError); }} className="btn bg-light-purple darken-on-hover w-100 text-white fw-600 py-2 mb-4">
+                                        <button
+                                            type="submit"
+                                            onClick={(event) => {
+                                                validateAndSubmit(event, setError);
+                                            }}
+                                            className="btn bg-light-purple darken-on-hover w-100 text-white fw-600 py-2 mb-4"
+                                        >
                                             Register
                                         </button>
 
                                         <div className="mx-auto">
-                                            <Link initialText="Already have an account?&nbsp;" linkText="Sign in" link="/" />
+                                            <Link
+                                                initialText="Already have an account?&nbsp;"
+                                                linkText="Sign in"
+                                                link="/"
+                                            />
                                         </div>
                                     </div>
                                 </form>
