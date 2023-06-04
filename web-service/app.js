0e5d11ef6f54bd44e7d8414b6d6e6fd4b5e18e88
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-06-04 15:01:52 +0300

    minor changes

--- web-service/app.js
+++ web-service/app.js
@@ -1,11 +1,18 @@
-import express from "express";
-import bodyParser from "body-parser";
 import { routerTokens } from "./routes/tokens.js";
 
-const app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/HemiDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+var app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/Tokens", routerTokens);
 
