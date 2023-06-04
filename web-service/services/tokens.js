25ca306286813a0386f4c840122ae2d565ce68d0
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-06-04 15:24:21 +0300

    added verifyToken started working on the users api

--- web-service/services/tokens.js
+++ web-service/services/tokens.js
@@ -1,10 +1,12 @@
 const User = require('../models/user');
 const jwt = require("jsonwebtoken");
 
+const secretKey = "Shhhhh...";
+
 const createToken = async (username, password) => {
     const user = await User.find({ username });
 
-    if (user === null) {
+    if (user.length === 0) {
         throw new Error('User not found');
     }
 
@@ -12,7 +14,16 @@ const createToken = async (username, password) => {
         throw new Error('Password is incorrect');
     }
 
-    return jwt.sign({ username }, "Shhhhh...")
+    return jwt.sign({ username }, secretKey)
+}
+
+const verifyToken = (token) => {
+    try {
+        const data = jwt.verify(token, secretKey);
+        return data;
+    } catch (err) {
+        throw new Error('Invalide token');
+    }
 }
 
 module.exports = { createToken };
--- web-service/services/users.js
+++ web-service/services/users.js
@@ -1,1 +1,29 @@
 const User = require('../models/user');
+const UserPass = require('../models/userPass');
+const UserPassName = require('../models/userPassName');
+
+const createUser = async (username, password, displayName, profilePic) => {    
+    const user = User.find({ username });
+
+    if (user.length > 0) {
+        throw new Error('User already exists');
+    }
+
+    await User.create({
+        username,
+        displayName,
+        profilePic,
+    });
+
+    await UserPass.create({
+        username,
+        password,
+    });
+
+    await UserPassName.create({
+        username,
+        password,
+        displayName,
+        profilePic,
+    });
+}
