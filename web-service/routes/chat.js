b731c091ab6eb0daf62905b211daaafb273a40c2
Jonathan Moiseyev <jonathanmoiseyev@outlook.com>
2023-06-05 12:41:28 +0300

    improved the readability of the code

--- web-service/routes/chat.js
+++ web-service/routes/chat.js
@@ -8,15 +8,9 @@ router.route("/Chats").get(tokenController.verifyToken, chatsController.getChats
 router.route("/Chats").post(tokenController.verifyToken, chatsController.postChats);
 
 router.route("/Chats/:id").get(tokenController.verifyToken, chatsController.getChat);
-router
-    .route("/Chats/:id")
-    .delete(tokenController.verifyToken, chatsController.deleteChat);
+router.route("/Chats/:id").delete(tokenController.verifyToken, chatsController.deleteChat);
 
-router
-    .route("/Chats/:id/Messages")
-    .post(tokenController.verifyToken, chatsController.addChatMessage);
-router
-    .route("/Chats/:id/Messages")
-    .get(tokenController.verifyToken, chatsController.getMessages);
+router.route("/Chats/:id/Messages").post(tokenController.verifyToken, chatsController.addChatMessage);
+router.route("/Chats/:id/Messages").get(tokenController.verifyToken, chatsController.getMessages);
 
 module.exports = router;
