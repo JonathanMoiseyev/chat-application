38dfa571d56fc5a8fa8c4280f08bfaf4bc563e31
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-06-06 15:13:26 +0300

    removed unnecessary code

--- web-service/controllers/token.js
+++ web-service/controllers/token.js
@@ -7,7 +7,7 @@ const createToken = async (req, res) => {
         const token = await tokenService.createToken(username, password);
         return res.status(200).send(token);
     } catch (error) {
-        return res.status(404).send("Invalid username or password");
+        return res.status(404).send("Incorrect username and/or password");
     } 
 }
 
@@ -17,10 +17,6 @@ const verifyToken = async (req, res, next) => {
     }
 
     const token = req.headers.authorization.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).send("No token provided");
-    }
 
     try {
         req.username = tokenService.verifyToken(token).username;
