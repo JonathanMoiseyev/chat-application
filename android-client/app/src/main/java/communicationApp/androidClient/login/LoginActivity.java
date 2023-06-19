7a93e608cd8d3ff3c3bfa4efe9566ed5db48fa2a
Jonathan Moiseyev <jonathanmoiseyev@outlook.com>
2023-06-19 16:54:50 +0300

    changed the login to use the http address from the strings.xml

--- android-client/app/src/main/java/communicationApp/androidClient/login/LoginActivity.java
+++ android-client/app/src/main/java/communicationApp/androidClient/login/LoginActivity.java
@@ -36,7 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        loginViewModel = new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        loginViewModel = new LoginViewModel(LoginRepository.getInstance(new LoginDataSource(getString(R.string.apiURL))));
 
         final EditText usernameEditText = binding.username;
         final EditText passwordEditText = binding.password;
