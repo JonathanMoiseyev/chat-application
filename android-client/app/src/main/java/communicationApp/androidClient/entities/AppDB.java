15b1770b41293b9dbc69dc87b8909e35db67243a
Jonathan Moiseyev <jonathanmoiseyev@outlook.com>
2023-06-22 00:25:02 +0300

    small fix to AppDB

--- android-client/app/src/main/java/communicationApp/androidClient/entities/AppDB.java
+++ android-client/app/src/main/java/communicationApp/androidClient/entities/AppDB.java
@@ -3,7 +3,7 @@ package communicationApp.androidClient.entities;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {CurrentUser.class, Chat.class, Settings.class}, version = 8)
+@Database(entities = {CurrentUser.class, Chat.class, Settings.class}, version = 9)
 public abstract class AppDB extends RoomDatabase {
     public abstract ChatDao chatDao();
     public abstract CurrentUserDao currentUserDao();
